eb45e46fcaf272672976b50650baa12ffa55db65
soldisnake1104 <cam.west1776@yahoo.com>
2026-07-09 20:40:12 -0400

    ham menu fix

--- script.js
+++ script.js
@@ -208,6 +208,9 @@ SimplerTools.escapeHtml = function (value) {
       '<svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"><path d="M6 6l12 12M18 6L6 18"/></svg>' +
       "</button>" +
       "</div>" +
+      '<ul class="nav-list">' +
+      '<li><a class="nav-link" href="' + base + 'index.html">Home Page</a></li>' +
+      "</ul>" +
       '<p class="nav-section-label">Tools</p>' +
       '<ul class="nav-list">' +
       navLinksHTML(TOOLS, toolHref) +
